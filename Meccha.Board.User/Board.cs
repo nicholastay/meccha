f2ac02dc152d71bd378fbcbfa3fdbbeafd2fd546
Nicholas Tay <nkt@outlook.kr>
2019-02-07 00:39:34 +1100

    Add a 'user' board for folder loaded sounds

---
+++ Meccha.Board.User/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meccha.Board.User
+{
+    public class Board : TemplateBoards.RandomizedSoundBaseBoard
+    {
+        public new static string BoardName => "User Folder (\\user)";
+
+        static string basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        static string userDir = Path.Combine(basePath, "user");
+
+        static string upPath = Path.Combine(userDir, "up");
+        static string downPath = Path.Combine(userDir, "down");
+
+        public Board() : base(tryLoadDir(upPath), tryLoadDir(downPath)) { }
+
+        static List<Stream> tryLoadDir(string dir)
+        {
+            if (!Directory.Exists(dir))
+                return null;
+
+            List<Stream> s = Directory.GetFiles(dir)
+                .Where(f => Path.GetExtension(f) == ".wav")
+                .Select(f => (Stream)File.Open(f, FileMode.Open))
+                .ToList();
+
+            if (s.Count == 0)
+                return null;
+            return s;
+        }
+    }
+}
